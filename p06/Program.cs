6ef69bbdf6b10ba88c964f08b38504723ca5fd72
al3gr3 <38777894+al3gr3@users.noreply.github.com>
2025-12-06 11:24:55 +0200

    6

--- p06/Program.cs
+++ p06/Program.cs
@@ -1,50 +1,39 @@
 ﻿var lines = File.ReadAllLines("TextFile1.txt");
+var actions = lines.Last().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
 
-SolveA(lines);
-SolveB(lines);
+SolveA();
+SolveB();
 
-void SolveB(string[] lines)
+void SolveB()
 {
     var temp = lines[..(lines.Length - 1)].Select(line => line.ToList()).ToList();
     temp = Transform(temp);
-    var allNumbers = temp.Select(x => (new string(x.ToArray())).Trim()).ToList();
-
-    var actions = lines.Last().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
+    var allNumbers = temp.Select(x => new string(x.ToArray()).Trim()).ToList();
 
     var result = 0L;
     for (int i = 0; i < actions.Count; i++)
     {
         var numbers = allNumbers.TakeWhile(x => !string.IsNullOrWhiteSpace(x)).Select(long.Parse).ToList();
         allNumbers = allNumbers.SkipWhile(x => !string.IsNullOrWhiteSpace(x)).Skip(1).ToList();
-        if (actions[i] == "+")
-            result += numbers.Sum();
-        else
-            result += numbers.Aggregate(1L, (a, b) => a * b);
+        result += AddOrMultiply(actions[i], numbers);
     }
     Console.WriteLine(result);
 }
 
-void SolveA(string[] lines)
+long AddOrMultiply(string v, List<long> numbers) => v == "+"
+    ? numbers.Sum()
+    : numbers.Aggregate(1L, (a, b) => a * b);
+
+void SolveA()
 {
     var numbers = lines[..(lines.Length - 1)].Select(line => line.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToList()).ToList();
     numbers = Transform(numbers);
-    
-    var actions = lines.Last().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
-    
 
-    var result = 0L;
-    for (int i = 0; i < actions.Count; i++)
-    {
-        if (actions[i] == "+")
-            result += numbers[i].Sum();
-        else
-            result += numbers[i].Aggregate(1L, (a, b) => a * b);
-    }
+    var result = actions.Zip(numbers).Sum(z => AddOrMultiply(z.First, z.Second));
     Console.WriteLine(result);
 }
 
-
-List<List<T>> Transform<T>(List<List<T>> numbers) 
+static List<List<T>> Transform<T>(List<List<T>> numbers) 
 {
     var result = new List<List<T>>();
 
@@ -52,9 +41,7 @@ List<List<T>> Transform<T>(List<List<T>> numbers)
     {
         var newRow = new List<T>();
         for (int i = 0; i < numbers.Count; i++)
-        {
             newRow.Add(numbers[i][j]);
-        }
         result.Add(newRow);
     }
     return result;
