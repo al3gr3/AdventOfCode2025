202e2c24f2ff479cea1dda8a0ac25ca209c96af3
al3gr3 <38777894+al3gr3@users.noreply.github.com>
2025-12-06 12:30:34 +0200

    GroupWithDelimiter

--- p06/Program.cs
+++ p06/Program.cs
@@ -1,37 +1,26 @@
 ﻿var lines = File.ReadAllLines("TextFile1.txt");
 var actions = lines.Last().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
 
-SolveA();
-SolveB();
+Print(SolveA());
+Print(SolveB());
 
-void SolveB()
-{
-    var temp = lines[..(lines.Length - 1)].Select(line => line.ToList()).ToList().Transpose();
-    var allNumbers = temp.Select(x => new string(x.ToArray()).Trim()).ToList();
+void Print(List<List<long>> list) => Console.WriteLine(actions.Zip(list).Sum(z => AddOrMultiply(z.First, z.Second)));
 
-    var result = 0L;
-    for (int i = 0; i < actions.Count; i++)
-    {
-        var numbers = allNumbers.TakeWhile(x => !string.IsNullOrWhiteSpace(x)).Select(long.Parse).ToList();
-        allNumbers = allNumbers.Skip(numbers.Count + 1).ToList();
-        result += AddOrMultiply(actions[i], numbers);
-    }
-    Console.WriteLine(result);
-}
+List<List<long>> SolveB() => lines[..(lines.Length - 1)]
+    .Select(line => line.ToList())
+    .ToList().Transpose()
+    .Select(x => new string(x.ToArray()).Trim())
+    .GroupWithDelimiter(x => string.IsNullOrWhiteSpace(x))
+    .Select(grp => grp.Select(long.Parse).ToList()).ToList();
+
 
 long AddOrMultiply(string v, List<long> numbers) => v == "+"
     ? numbers.Sum()
     : numbers.Aggregate(1L, (a, b) => a * b);
 
-void SolveA()
-{
-    var numbers = lines[..(lines.Length - 1)]
-        .Select(line => line.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToList())
-        .ToList().Transpose();
-
-    var result = actions.Zip(numbers).Sum(z => AddOrMultiply(z.First, z.Second));
-    Console.WriteLine(result);
-}
+List<List<long>> SolveA() => lines[..(lines.Length - 1)]
+    .Select(line => line.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToList())
+    .ToList().Transpose();
 
 public static class Extensions
 {
@@ -46,6 +35,20 @@ public static class Extensions
                 newRow.Add(numbers[i][j]);
             result.Add(newRow);
         }
+        return result;
+    }
+
+    public static List<List<T>> GroupWithDelimiter<T>(this IEnumerable<T> numbers, Func<T, bool> isDelimiter)
+    {
+        var result = new List<List<T>>();
+
+        while (numbers.Any())
+        {
+            var grp = numbers.TakeWhile(x => !isDelimiter(x)).ToList();
+            result.Add(grp);
+            numbers = numbers.Skip(grp.Count + 1).ToList();
+        }
+
         return result;
     }
 }
