cdf9f7750ce757061fd650424bb608a604c37028
al3gr3 <38777894+al3gr3@users.noreply.github.com>
2025-12-07 12:08:32 +0200

    7

--- p07/Program.cs
+++ p07/Program.cs
@@ -1,7 +1,4 @@
-﻿var lines = File.ReadAllLines("TextFile1.txt")
-    .Select(line => line.ToList()).ToList()
-    .Transpose()
-    .Select(x => new string(x.ToArray())).ToArray();
+﻿var lines = File.ReadAllLines("TextFile1.txt");
 
 SolveA();
 SolveB();
@@ -12,26 +9,26 @@ void SolveB()
     {
         new Beam
         {
-            Y = Array.FindIndex(lines, x => x.StartsWith("S")),
-            X = 0,
+            X = Array.FindIndex(lines.First().ToArray(), x => x =='S'),
+            Y = 0,
             Count = 1
         }
     }.ToList();
 
     var res = 0;
-    for (var i = 0; i < lines.First().Length - 1; i++)
+    for (var i = 0; i < lines.Length - 1; i++)
     {
         var newBeams = new List<Beam>();
         foreach (var beam in beams)
         {
-            var currentChar = lines[beam.Y][beam.X + 1];
+            var currentChar = lines[beam.Y + 1][beam.X];
             if (currentChar == '^')
             {
                 res++;
                 AddWithCount(newBeams, new Beam
                 {
-                    X = beam.X + 1,
-                    Y = beam.Y - 1,
+                    X = beam.X - 1,
+                    Y = beam.Y + 1,
                     Count = beam.Count,
                 });
                 AddWithCount(newBeams, new Beam
@@ -44,8 +41,8 @@ void SolveB()
             else
                 AddWithCount(newBeams, new Beam
                 {
-                    X = beam.X + 1,
-                    Y = beam.Y,
+                    X = beam.X,
+                    Y = beam.Y + 1,
                     Count = beam.Count
                 });
         }
@@ -53,6 +50,7 @@ void SolveB()
     }
 
     Console.WriteLine(beams.Sum(x => x.Count));
+    Console.WriteLine(res);
 }
 
 void Add(List<Beam> newBeams, Beam point)
@@ -77,65 +75,43 @@ void SolveA()
     {
         new Beam
         {
-            Y = Array.FindIndex(lines, x => x.StartsWith("S")),
-            X = 0
+            X = Array.FindIndex(lines.First().ToArray(), x => x =='S'),
+            Y = 0,
         }
     }.ToList();
 
     var res = 0;
-    for (var i = 0; i < lines.First().Length - 1; i++)
+    for (var i = 0; i < lines.Length - 1; i++)
     {
         var newBeams = new List<Beam>();
         foreach (var beam in beams)
         {
-            var currentChar = lines[beam.Y][beam.X + 1];
+            var currentChar = lines[beam.Y + 1][beam.X];
             if (currentChar == '^')
             {
                 res++;
                 Add(newBeams, new Beam
                 {
                     X = beam.X + 1,
-                    Y = beam.Y - 1
+                    Y = beam.Y + 1
                 });
                 Add(newBeams, new Beam
                 {
-                    X = beam.X + 1,
+                    X = beam.X - 1,
                     Y = beam.Y + 1
                 });
             }
             else
                 Add(newBeams, new Beam
                 {
-                    X = beam.X + 1,
-                    Y = beam.Y
+                    X = beam.X,
+                    Y = beam.Y + 1,
                 });
         }
         beams = newBeams;
     }
 
     Console.WriteLine(res);
-}
-
-public static class Extensions
-{
-    public static List<List<T>> Transpose<T>(this List<List<T>> numbers) =>
-        Enumerable.Range(0, numbers.First().Count).Select(j =>
-            Enumerable.Range(0, numbers.Count).Select(i => numbers[i][j]).ToList()
-        ).ToList();
-
-    public static List<List<T>> GroupWithDelimiter<T>(this IEnumerable<T> numbers, Func<T, bool> isDelimiter)
-    {
-        var result = new List<List<T>>();
-
-        while (numbers.Any())
-        {
-            var grp = numbers.TakeWhile(x => !isDelimiter(x)).ToList();
-            result.Add(grp);
-            numbers = numbers.Skip(grp.Count + 1).ToList();
-        }
-
-        return result;
-    }
 }
 
 class Beam
